92b956f8ae08c5b72dbfc7137184f89c379afcaa
Shivani Yadav <shivaaniyadav31@gmail.com>
2025-06-27 18:17:35 +0530

    //

---
+++ pattern.py
@@ -0,0 +1,20 @@
+from turtle import *
+# pentagon
+pencolor('pink')
+speed('slowest')
+fillcolor('yellow')
+begin_fill()
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+end_fill()
+hideturtle()
+
+mainloop()  #to hold the output screen
